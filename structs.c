7c1df8402508ac72b8c7dc51b6e002b33b303870
Estuardo Ruiz <eruiz@healthcare.com>
2018-12-09 15:24:16 -0600

    Added structs

---
+++ structs.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define EXIT "exit"
+#define MKDISK "mkdisk"
+
+struct Partition {
+    char part_status[1];
+    char part_type[1];
+    char part_fit[1];
+    int part_start;
+    int part_size;
+    char part_name[16];
+};
+
+struct MasterBootRecord {
+    int mbr_tamano;
+    char mbr_fecha_creacion[10];
+    int mbr_disk_signature;
+    char disk_fit[1];
+    struct Partition *mbr_partition_1;
+    struct Partition *mbr_partition_2;
+    struct Partition *mbr_partition_3;
+    struct Partition *mbr_partition_4;
+};
+
+struct ExtendedBootRecord {
+    char part_status[1];
+    char part_fit[1];
+    int part_start;
+    int part_size;
+    int part_next;
+    char part_name[16];
+};
+
+int main() {
+
+    return 0;
+}
